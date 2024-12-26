067d1943b335ee63366c9c746a618fc9919a38c1
Tahmid Rayhan <107885910+thamidrayhan943@users.noreply.github.com>
2024-12-25 20:16:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.getElementById('check-schedule').addEventListener('click', function () {
     const resetTime = document.getElementById('reset-time').value;
+    const timeZone = document.getElementById('time-zone').value;
 
     if (!resetTime) {
         alert("Please enter a valid reset time.");
         return;
     }
 
-    fetch(`https://tahmidr20.pythonanywhere.com/next_appearance?reset_time=${resetTime}`)
+    fetch(`http://127.0.0.1:5000/next_appearance?reset_time=${resetTime}&time_zone=${timeZone}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -32,7 +33,7 @@ document.getElementById('check-schedule').addEventListener('click', function () 
                     <p class="appearance-location"><i class="fas fa-map-marker-alt"></i> <strong>Location:</strong> ${data.realm}</p>
                 `;
             } else {
-                document.getElementById('next-appearance').innerText = "Server is offline please let the owner know.";
+                document.getElementById('next-appearance').innerText = "Sorry, something went wrong.";
             }
         })
         .catch(error => {
